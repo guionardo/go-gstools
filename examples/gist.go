e84479965837de23d8b40e5b5efd2250f3d3d2a2
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2022-12-30 17:41:49 -0300

    Gist docs

---
+++ examples/gist.go
@@ -0,0 +1,38 @@
+package examples
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/guionardo/go-gstools/gist"
+)
+
+func main() {
+	var (
+		err error
+		ctx context.Context
+	)
+
+	// Create a local gist instance from files in a folder
+	myGist, err := gist.CreateGistFromLocalFolder("My gist", "/home/user/gist")
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a new git context, using a github token
+	ctx, err = gist.NewGitContext("github_token", context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	// Synchronize the local gist with the github gist
+	remoteGist, action, err := gist.SyncGistFiles(ctx, myGist, "/home/user/gist")
+	if err != nil {
+		panic(err)
+	}
+	// Action can be Download, Upload or NoAction
+	fmt.Printf("Action: %v\n", action)
+
+	// Print the remote gist ID
+	fmt.Printf("Remote gist ID: %s\n", *remoteGist.ID)
+}
